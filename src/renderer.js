387be6aa511c6d1124dc6874e2e8022500efedfc
Rictus <contact.dylanleroux@gmail.com>
2016-11-13 17:46:49 +0100

    Using dropzone through electron

--- src/renderer.js
+++ src/renderer.js
@@ -1,62 +1,22 @@
+var Dropzone = require('./dropzone.js');
 var onBrowser = typeof require !== "function";
 if (!onBrowser) {
     var runner = require('./ImageOCR')();
 }
 
 var domElements = {
-    container: document.getElementsByClassName('imageDrop-mainContainer')[0],
-    input: document.getElementById("imageDropInput"),
-    imgContainer: document.getElementsByClassName('imageDrop-imgCont')[0],
-    text: document.getElementsByClassName('imageDrop-textFound')[0],
-    img: document.getElementsByClassName('imageDrop-img')[0],
-    elementExample: document.createElement('span'),
-    retryButton: document.getElementsByClassName('retryButton')[0],
-    launchButton: document.getElementsByClassName('launchButton')[0],
-    form: document.getElementsByClassName('imageDrop-form')[0],
-    textExplain: document.getElementsByClassName('imageDrop-explain')[0],
-    putImage: function (path) {
-        this.img.setAttribute('src', path);
-        this.container.classList.add("imageSelected");
-    },
-    putText: function (textFound) {
-        if (typeof textFound === "string" && textFound.length > 0) {
-            this.text.innerHTML = textFound;
-            this.container.classList.add("textFound");
-        } else {
-            console.log("No text found.");
-        }
-    },
-    setLoading: function () {
-        this.container.classList.add("loading");
-    },
-    unsetLoading: function () {
-        this.container.classList.remove("loading");
-    },
-    retry: function () {
-        this.unsetLoading();
-        this.img.setAttribute('src', '');
-        this.container.classList.remove("imageSelected");
-        this.container.classList.remove("textFound");
-    },
-    launch: function () {
-        var that = this;
-        if (domElements.input.files.length > 0) {
-            var file = domElements.input.files[0];
-            domElements.putImage(file.path);
-            domElements.setLoading();
-            runner.getText(file.path, file.name, function (txt) {
-                that.unsetLoading();
-                domElements.putText(txt);
-            });
-        } else {
-            console.error("No file selected.");
-        }
-    }
+    imageDropMainContainer: document.getElementById("imageDropMainContainer")
 };
 
 var initDropzone = function () {
-    Dropzone.options.imageDropMainContainer = {
-        url: "/imageUpload",
+    var url;
+    if (onBrowser) {
+        url = "/imageUpload";
+    } else {
+        url = "/WIP"; // WIP Which URL when using electron ?
+    }
+    var opts = {
+        url: url,
         paramName: "image",
         acceptedFiles: "image/*",
         addRemoveLinks: true,
@@ -67,6 +27,7 @@ var initDropzone = function () {
             pE.appendChild(el);
         }
     };
+    new Dropzone(domElements.imageDropMainContainer, opts);
 };
 
 var initListeners = function () {
@@ -75,20 +36,6 @@ var initListeners = function () {
         console.info("Drag and drop feature initialized.");
     } else {
         console.error("Dropzone library is not loaded.");
-    }
-
-    if (!onBrowser) {
-        domElements.launchButton.addEventListener('click', function () {
-            domElements.launch();
-        });
-        domElements.retryButton.addEventListener('click', function () {
-            domElements.retry();
-        });
-        domElements.input.addEventListener('change', function () {
-            var file = domElements.input.files[0];
-            console.log(file);
-            domElements.putImage(file.path);
-        });
     }
 };
 
